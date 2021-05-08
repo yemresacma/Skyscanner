0caa98fbd8b9cb2ae0ed818947968e0a5a0a7811
Yunus Emre <y.emre_sacma@hotmail.com>
2021-05-08 10:06:31 +0300

    init spring project

---
+++ src/main/java/com/finartz/skyscanner/SkyscannerApplication.java
@@ -0,0 +1,13 @@
+package com.finartz.skyscanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyscannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyscannerApplication.class, args);
+	}
+
+}
