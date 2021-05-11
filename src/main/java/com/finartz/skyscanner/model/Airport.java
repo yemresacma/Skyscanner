12f81137bdd6ee98d89e17acf6a901f474ba01ed
Yunus Emre <y.emresacma@gmail.com>
2021-05-11 09:54:34 +0300

    bug fix for previous commit

--- src/main/java/com/finartz/skyscanner/model/Airport.java
+++ src/main/java/com/finartz/skyscanner/model/Airport.java
@@ -7,7 +7,11 @@ import javax.persistence.*;
 public class Airport extends NamedEntity {
     public Airport() {
     }
-
+    
+    public Airport(String name) {
+        this.setName(name);
+    }
+    
     @Override
     public String toString() {
         return "Airport [id=" + super.getId() + ", airportName=" + super.getName() + "]";
